08fc593ca222f07017e4ec5b6eea8d5d9768b39d
Devon Bahary <devonbahary@gmail.com>
2025-12-06 23:20:18 -0800

    chore: removes unused window global exposure

--- packages/rmmz-plugins/physick/src/index.js
+++ packages/rmmz-plugins/physick/src/index.js
@@ -1,9 +1,6 @@
 // Import collision-detection library
 import * as CollisionDetection from 'physics-engine';
 
-// Make available globally (*may be* required for RMMZ environment; TODO: confirm this)
-window.CollisionDetection = CollisionDetection;
-
 const pluginParams = PluginManager.parameters('Physick');
 
 import './objects/Game_CharacterBase';
